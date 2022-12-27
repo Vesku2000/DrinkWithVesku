0e1356e8a967eeb0cfb15dd21f18d79d50ff4c45
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-12-27 21:58:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const form = document.querySelector('.form');
+  const playerList = document.querySelector('.player-list');
+
+  // Load any existing players from local storage
+  const players = JSON.parse(localStorage.getItem('players')) || [];
+  players.forEach((player) => {
+    const playerElement = document.createElement('div');
+    playerElement.classList.add('player');
+    playerElement.innerHTML = `
+      <img src="./img/${player.avatar}" class="avatar">
+      <span>${player.name}</span>
+    `;
+    playerList.appendChild(playerElement);
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Add the new player to the list
+    const name = form.elements.name.value;
+    const avatar = form.elements.avatar.value;
+    players.push({ name, avatar });
+
+    const playerElement = document.createElement('div');
+    playerElement.classList.add('player');
+    playerElement.innerHTML = `
+      <img src="./img/${avatar}" class="avatar">
+      <span>${name}</span>
+    `;
+    playerList.appendChild(playerElement);
+
+    // Save the players to local storage
+    localStorage.setItem('players', JSON.stringify(players));
+
+    form.reset();
+  });
+
+
+  let selectedPlayer;
+
+  playerList.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('player')) {
+      if (selectedPlayer === target) {
+        // Deselect the player if it is already selected
+        selectedPlayer = null;
+        target.style.boxShadow = 'none';
+      } else {
+        if (selectedPlayer) {
+          selectedPlayer.style.boxShadow = 'none';
+        }
+        selectedPlayer = target;
+        target.style.boxShadow = '0 0 10px red';
+      }
+    }
+  });
+
+  const deleteButton = document.querySelector('#delete-button');
+  deleteButton.addEventListener('click', () => {
+    // Delete the selected player from the list
+    if (selectedPlayer) {
+      const index = Array.from(playerList.children).indexOf(selectedPlayer);
+      players.splice(index, 1);
+      selectedPlayer.remove();
+      selectedPlayer = null;
+    }
+
+    // Update the players in local storage
+    localStorage.setItem('players', JSON.stringify(players));
+  });
