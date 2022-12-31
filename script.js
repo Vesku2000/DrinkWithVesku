fa17a46710dd79769ceeb75f2a5afda28228295f
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-12-31 11:01:40 +0200

    adding more questions and changing question size

--- script.js
+++ script.js
@@ -123,7 +123,37 @@ const questions = [
   "What is your favorite color?",
   "What is your favorite food?",
   "What is your favorite hobby?",
-  "What is your favorite movie?"
+  "What is your favorite movie?",
+  "Have you ever had the worst hangover story?",
+"Have you ever kissed someone of the same gender while drunk?",
+"Have you ever sent a drunk text you regret?",
+"Have you ever fallen asleep somewhere strange while drunk?",
+"Have you ever thrown up while drunk?",
+"Have you ever done something embarrassing while drunk that you still remember?",
+"Have you ever woken up in a different location than where you fell asleep?",
+"Have you ever done something you swore you would never do while drunk?",
+"Have you ever hooked up with someone you barely knew while drunk?",
+"Have you ever called an ex while drunk?",
+"Have you ever forgotten something important that happened while drunk?",
+"Have you ever eaten something weird while drunk?",
+"Have you ever danced on a bar while drunk?",
+"Have you ever gotten into a fight while drunk?",
+"Have you ever stolen something while drunk (even if it was just a small item)?",
+"Have you ever gone skinny dipping while drunk?",
+"Have you ever gotten a tattoo while drunk?",
+"Have you ever made a prank call while drunk?",
+"Have you ever gone streaking while drunk?",
+"Have you ever tried to cook while drunk and ended up setting something on fire?",
+"Have you ever tried to play a sport while drunk and ended up hurting yourself?",
+"Have you ever tried to sing while drunk and ended up sounding terrible?",
+"Have you ever tried to play a musical instrument while drunk and ended up making a lot of noise?",
+"Have you ever tried to do karaoke while drunk and ended up making a fool of yourself?",
+"Have you ever tried to do stand-up comedy while drunk and ended up bombing?",
+"Have you ever tried to do a magic trick while drunk and ended up failing?",
+"Have you ever tried to play a video game while drunk and ended up losing miserably?",
+"Have you ever tried to play a board game while drunk and ended up cheating?",
+"Have you ever tried to do a puzzle while drunk and ended up giving up?",
+"Have you ever tried to do a science experiment while drunk and ended up causing a disaster?"
 ];
 
 function shufleQuestions() {
@@ -137,12 +167,14 @@ startGameButton.addEventListener("click", () => {
   elements.forEach((element) => {
     element.classList.add("hidden");
   });
+  shufleQuestions();
   showButton();
   redCard.style.display = "block";
   playerdiv.style.display = "block";
   document.body.style.backgroundColor = ""
   const quest = questions.splice(0, 1)[0];
   p.textContent = quest;
+  console.log(p.textContent.length);
   redCard.appendChild(p);
   nameText.textContent = getRandomPlayerName();
   console.log(nameText);
@@ -204,12 +236,15 @@ function showButton() {
 function updateRedCard() {
   console.log("workkii");
   showButton();
+  console.log(p.textContent.length);
   playerdiv.style.display = "block";
   document.body.style.backgroundColor = "black";
   redCard.style.display = "block";
   const quest = questions.splice(0, 1)[0];
   p.textContent = quest;
+  checkTheLongOfQuestion(p.textContent);
   redCard.appendChild(p);
+  
   nameText.textContent = getRandomPlayerName();
   console.log(nameText);
   redCard.appendChild(nameText);
@@ -229,5 +264,18 @@ function updateRedCard() {
 
 const button = document.getElementById("myButton");
 button.addEventListener("click", updateRedCard);
+
+function checkTheLongOfQuestion(question){
+  if(question.length >= 80){
+    const text = document.getElementById("question-text");
+    text.style.fontSize = "20px";
+  }else if(question.length >= 50){
+    const text = document.getElementById("question-text");
+    text.style.fontSize = "32px";
+  }else{
+    const text = document.getElementById("question-text");
+    text.style.fontSize = "40px";
+  }
+}
 
 
